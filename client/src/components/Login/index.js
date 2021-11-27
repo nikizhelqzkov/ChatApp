2b1b75703e4228495b931c1e06709eb763d857fb
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-11-28 00:33:00 +0200

    Adding Login component Adding login component for logging the user to use the app

---
+++ client/src/components/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { LoginContainer } from "./Login.Styles";
+
+
+const Login = () => {
+ 
+  return (
+    <LoginContainer>
+      <h1>Welcome to the Chat App</h1>
+      <h2>Author Nikolay Zhelyazkov && Silvia Deyanova</h2>
+    
+    </LoginContainer>
+  );
+};
+
+export default Login;
