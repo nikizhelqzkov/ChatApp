d72a0a7326efe1ae9a7253390f1982a8f297e6df
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-12-03 01:20:03 +0200

    Adding profile menu with google logout Adding profile options with logout button,which is logging the app + adding some make-styles for the material ui components

--- client/src/components/menu/header/Header.Styles.js
+++ client/src/components/menu/header/Header.Styles.js
@@ -4,10 +4,11 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   -webkit-box-shadow: 0px 0px 5px 1px #3d3c3d;
-    box-shadow: 0px 0px 5px 1px #3d3c3d;
+  box-shadow: 0px 0px 5px 1px #3d3c3d;
   & > * {
     margin-right: 10px;
     padding: 2px 5px;
+    text-align: center;
   }
   .profileLogo {
     width: 30px;
@@ -16,7 +17,10 @@ export const Wrapper = styled.div`
     padding: 10px;
     margin-right: 0;
   }
-
+  .MuiMenuItem-root {
+    text-align: center !important;
+    background: red !important;
+  }
   .menuMore {
     margin-left: auto;
     cursor: pointer;
--- client/src/components/menu/header/Header.js
+++ client/src/components/menu/header/Header.js
@@ -2,9 +2,30 @@ import React, { useContext, useState } from "react";
 import { AccountContext } from "../../../context/AccountProvider";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { Wrapper } from "./Header.Styles";
-import { Menu, MenuItem } from "@material-ui/core";
+import { Menu, MenuItem, makeStyles } from "@material-ui/core";
+import { GoogleLogout } from "react-google-login";
+import { clientID } from "../../data/googleApi";
+const useStyle = makeStyles({
+  menuItem: {
+    fontSize: 14,
+    backgroundColor:'#fff !important',
+    "& > *": {
+      textAlign: "center",
+      width: "100%",
+    },
+    "&:hover":{
+      backgroundColor:' #e1dad8  !important'
+    },
+  },
+  logout: {
+    border: "none!important",
+    boxShadow: "none!important",
+  },
+});
+
 const Header = () => {
-  const { account } = useContext(AccountContext);
+  const classes = useStyle();
+  const { account, setAccount } = useContext(AccountContext);
   const [open, setOpen] = useState(false);
   const handleClose = () => {
     setOpen(false);
@@ -12,11 +33,18 @@ const Header = () => {
   const handleClick = (event) => {
     setOpen(event.currentTarget);
   };
+  const onSignoutSuccess = () => {
+    alert(`You left from the account of ${account.givenName}`);
+    console.clear();
+    setAccount("");
+  };
+
   return (
     <Wrapper>
       <img src={account.imageUrl} alt="profile logo" className="profileLogo" />
       <h2>Chats</h2>
-      <MoreHorizIcon className="menuMore" onClick={handleClick} />
+      <MoreHorizIcon className="menuMore" onClick={handleClick}  aria-controls="fade-menu"
+        aria-haspopup="true"/>
       <Menu
         id="accountMenu"
         aria-labelledby="accountMenu"
@@ -33,8 +61,17 @@ const Header = () => {
           horizontal: "left",
         }}
       >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem className={classes.menuItem} onClick={handleClose}>
+          <span>Profile</span>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <GoogleLogout
+            clientId={clientID}
+            buttonText="Logout"
+            onLogoutSuccess={onSignoutSuccess}
+            className={classes.logout}
+          />
+        </MenuItem>
       </Menu>
     </Wrapper>
   );
