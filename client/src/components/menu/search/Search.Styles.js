a73bc1a5d3a7f896561470309e88e254c6b470fb
nikizhelqzkov <nikizhelqzkov@gmail.com>
2021-12-03 18:22:56 +0200

    Front end design of the search component Adding only front end design for the search component

---
+++ client/src/components/menu/search/Search.Styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const SearchBox = styled.div`
+  box-shadow: 0 4px 7px -2px #3d3c3d;
+  padding: 15px 10px;
+  display: flex;
+  align-items: center;
+  .searchBox__inner {
+    margin: 0 3px;
+    background-color: #fff;
+    width: 100%;
+    padding: 3px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    min-height: 23px;
+    & > input {
+      border: 0;
+      outline: none;
+      font-size: 14px;
+      margin-left: 5px;
+      cursor: pointer;
+    }
+  }
+`;
--- client/src/components/menu/search/Search.js
+++ client/src/components/menu/search/Search.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { SearchBox } from "./Search.Styles";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Search = () => {
-  return <div>Hello search</div>;
+  return (
+    <SearchBox className="searchBox">
+      <div className="searchBox__inner">
+        <SearchIcon fontSize="small" />
+        <input type="text" placeholder="Search contacts"/>
+      </div>
+    </SearchBox>
+  );
 };
 
 export default Search;
