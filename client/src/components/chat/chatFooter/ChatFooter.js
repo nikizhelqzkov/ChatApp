0b76045c5d7e73f6cccd318df11d3ab3df9eeb2e
silviaDeyanova <63786822+silviaDeyanova@users.noreply.github.com>
2021-12-16 00:58:56 +0200

    Added emoji picker Installed emoji-picker-react and created handler for it

--- client/src/components/chat/chatFooter/ChatFooter.js
+++ client/src/components/chat/chatFooter/ChatFooter.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, MessageBox } from "./ChatFooter.Styles";
 import { SentimentSatisfiedAltRounded } from "@material-ui/icons";
 import { AddAPhotoOutlined } from "@material-ui/icons";
 import { SendRounded } from "@material-ui/icons";
+import Picker from "emoji-picker-react";
+
 const ChatFooter = () => {
+  const [emojiOpen, setEmojiOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const emojiHandler = () => {
+    setEmojiOpen((prev) => !prev);
+  };
+
+  const onEmojiClick = (event, emojiObject) => {
+    emojiHandler();
+    setMessage((prev) => prev + emojiObject.emoji);
+  };
+
+  const handleKeyDown= (e) => {
+    if (e.key === 'Enter') {
+      console.log('do validate'); 
+      //submitHandler(); + clear message
+    }
+  }
+
   return (
     <Container>
-      <SentimentSatisfiedAltRounded className="chatIcons" fontSize="medium" />
+      <SentimentSatisfiedAltRounded
+        className="chatIcons"
+        fontSize="medium"
+        onClick={emojiHandler}
+      />
+      {emojiOpen && (
+        <Picker
+          pickerStyle={{ position: "absolute", bottom: "4rem" }}
+          onEmojiClick={onEmojiClick}
+        />
+      )}
       <AddAPhotoOutlined className="chatIcons" fontSize="medium" />
-      <MessageBox type="text" placeholder="Write a message" />
+      <MessageBox
+        type="text"
+        placeholder="Write a message"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+        onKeyDown={handleKeyDown}
+      />
       <SendRounded
         className="chatIcons"
         fontSize="medium"
